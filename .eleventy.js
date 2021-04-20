b938be9e5a03cde8da6aef5cda439cbf92c99b1b
Frederik De Bleser <frederik@debleser.be>
2021-04-20 15:05:54 +0200

    Also copy yml file to admin.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("admin/config.yml");
 };
