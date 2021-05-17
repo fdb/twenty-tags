a091245f3d9ce817551c79d7a09b19ffa575e87c
Frederik De Bleser <frederik@debleser.be>
2021-05-17 15:47:01 +0200

    Made some changes lol

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("admin/config.yml");
 };
